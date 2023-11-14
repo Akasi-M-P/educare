e71b8561c157a5dc838afc449e1e342c9a84aae3
“Peter-Akasi-Mba” <awintechworld@gmail.com>
2023-11-14 23:04:51 +0000

    finished working on responsive navbar

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 const mobileBar = document.querySelector(".mobile-nav");
 const mobileDropdown = document.querySelector(".mobileNav");
+const hideBar = document.querySelector(".mobile-nav");
+const showCloseNav = document.querySelector(".close-nav");
 
 mobileBar.addEventListener("click", function () {
   mobileDropdown.classList.add("mobile-dropdown");
+  hideBar.classList.add("hide-bar");
+  showCloseNav.classList.add("display-close-nav");
+});
+
+showCloseNav.addEventListener("click", function () {
+  mobileDropdown.classList.remove("mobile-dropdown");
+  showCloseNav.classList.remove("display-close-nav");
+  hideBar.classList.remove("hide-bar");
 });
